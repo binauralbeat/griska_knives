e540aa68535b8a52c0a77cc9f0d70f16bfdbd797
dan beecroft <binauralbeat001@gmail.com>
2019-05-05 07:33:41 -0500

    created modal for knife image

---
+++ env_griskaknives/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\binau\workspace\griska_knives\sites\griska_knives\env_griskaknives\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
